984bad5e0d1366e757800598f4f2308797c4695e
Argeriskul <dmitry.argeriskul@gmail.com>
2022-01-03 15:43:54 +0400

    introduced ConsoleController

--- src/main/java/ru/solarsystem/service/PlanetsService.java
+++ src/main/java/ru/solarsystem/service/PlanetsService.java
@@ -10,11 +10,8 @@ import java.util.List;
 @Component
 public class PlanetsService {
 
-    public void getPlanetsDistance(Planet planet1, Planet planet2) {
-
-        System.out.println("Расстояние между планетами " + planet1.getName() + " и " + planet2.getName() + ": "
-                + new DecimalFormat( "#,###" ).format(Math.abs(planet1.getDistance() - planet2.getDistance())) + " км.");
-
+    public long getPlanetsDistance(Planet planet1, Planet planet2) {
+        return Math.abs(planet1.getDistance() - planet2.getDistance());
     }
 
 
