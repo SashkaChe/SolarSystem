69e8b20d29bbc483bebbce30c95fffd26b7bbe9a
Alexsandr Chevtaev <alex-chevtaev@yandex.ru>
2022-01-26 08:54:20 +0300

    Расстояние между планетами

--- src/main/java/ru/solarsystem/controller/WebController.java
+++ src/main/java/ru/solarsystem/controller/WebController.java
@@ -26,12 +26,29 @@ public class WebController {
         return "sundistance";
     }
 
+    @GetMapping("/planetdistance")
+    public String planetDistancePageGet(Model model) {
+        model.addAttribute("allPlanets", planetsService.findAllPlanets());
+        return "planetdistance";
+    }
+
 
     @PostMapping("/sundistance")
     public String planetList(@RequestParam("planet") int num, Model model) {
         model.addAttribute("allPlanets", planetsService.findAllPlanets());
         model.addAttribute("planet", planetsService.findByIndex(num));
         return "sundistance";
+    }
+
+
+    @PostMapping("/planetdistance")
+    public String planetList(@RequestParam("planet1") int planet1, @RequestParam("planet2") int planet2, Model model) {
+        model.addAttribute("allPlanets", planetsService.findAllPlanets());
+        model.addAttribute("planet_1", planetsService.findByIndex(planet1));
+        model.addAttribute("planet_2", planetsService.findByIndex(planet2));
+        model.addAttribute("planet_mindistance", planetsService.getMinPlanetsDistance(planetsService.findByIndex(planet1), planetsService.findByIndex(planet2)));
+        model.addAttribute("planet_maxdistance", planetsService.getMaximumPlanetDistance(planetsService.findByIndex(planet1), planetsService.findByIndex(planet2)));
+        return "planetdistance";
     }
 
 }
