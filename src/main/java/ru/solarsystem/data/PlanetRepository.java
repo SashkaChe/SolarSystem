bdb938462f0b15babae0fb38636abad7226a17bd
Argeriskul <dmitry.argeriskul@gmail.com>
2022-02-19 02:28:52 +0400

    added hint for alternative

--- src/main/java/ru/solarsystem/data/PlanetRepository.java
+++ src/main/java/ru/solarsystem/data/PlanetRepository.java
@@ -5,7 +5,7 @@ import ru.solarsystem.model.Planet;
 import java.util.List;
 
 public interface PlanetRepository {
-    Planet findByIndex(int id);
+    Planet findByIndex(int id); // use Optional<Planet> findById instead
     Planet findByName(String name);
     List<Planet> findAll();
 }
--- src/main/java/ru/solarsystem/service/PlanetsService.java
+++ src/main/java/ru/solarsystem/service/PlanetsService.java
@@ -3,7 +3,6 @@ package ru.solarsystem.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.solarsystem.data.PlanetRepository;
-import ru.solarsystem.data.PlanetsRepo;
 import ru.solarsystem.model.Planet;
 import ru.solarsystem.model.Satellite;
 
