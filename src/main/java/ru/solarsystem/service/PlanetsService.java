73040fbe96f76dd6d48db547841ac6d7cbac6703
Alexsandr Chevtaev <alex-chevtaev@yandex.ru>
2022-02-19 13:34:43 +0300

    bind to the data base

--- src/main/java/ru/solarsystem/service/PlanetsService.java
+++ src/main/java/ru/solarsystem/service/PlanetsService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.solarsystem.data.PlanetsRepo;
 import ru.solarsystem.model.Planet;
+import ru.solarsystem.model.Satellite;
 
 import java.util.List;
 
