4c70d2272b343a081c69a97849568748a142cecd
Argeriskul <dmitry.argeriskul@gmail.com>
2022-02-20 15:44:59 +0400

    changed PlanetsRepo to extend JpaRepository

--- src/main/java/ru/solarsystem/data/PlanetsRepo.java
+++ src/main/java/ru/solarsystem/data/PlanetsRepo.java
@@ -1,9 +1,8 @@
 package ru.solarsystem.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import ru.solarsystem.model.Planet;
 
-public interface PlanetsRepo extends CrudRepository<Planet, Integer> {
+public interface PlanetsRepo extends JpaRepository<Planet, Integer> {
     Planet findByName(String name);
 }
