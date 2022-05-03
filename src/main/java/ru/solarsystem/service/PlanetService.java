84ef0463cad3c1e71429eab512bfedff1fcd0364
Alexsandr Chevtaev <alex-chevtaev@yandex.ru>
2022-05-03 13:37:58 +0300

    Added PlanetRestController

--- src/main/java/ru/solarsystem/service/PlanetService.java
+++ src/main/java/ru/solarsystem/service/PlanetService.java
@@ -1,13 +1,19 @@
 package ru.solarsystem.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.solarsystem.data.PlanetRepo;
+import ru.solarsystem.model.MessageDto;
 import ru.solarsystem.model.Planet;
 import ru.solarsystem.model.Satellite;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class PlanetService {
@@ -54,6 +60,37 @@ public class PlanetService {
 
     public double compareSizePlanets(Planet planet1, Planet planet2) {
         return (double) planet1.getDiameter()/planet2.getDiameter();
+    }
+
+
+    public HashMap<String, String> mapRealValue(MessageDto message, HashMap<String, String> mapKey, HashMap<String, String> mapClean) throws JsonProcessingException {
+
+        String messageToStringJson = new ObjectMapper().writeValueAsString(message);
+
+        List<String> listMessage = Arrays.asList(messageToStringJson.split("[\":},{]")).stream().filter(item -> !item.equals("")).collect(Collectors.toList());
+
+        for (Map.Entry<String, String> mapKeyObj : mapKey.entrySet()) {
+            for (Map.Entry<String, String> mapCleanMarker : mapClean.entrySet()) {
+
+                if (mapKeyObj.getKey().equalsIgnoreCase(mapCleanMarker.getKey())) {
+
+                    List<String> subListMessage = listMessage.subList(listMessage.indexOf(mapKeyObj.getValue()), listMessage.size());
+                    int x = subListMessage.indexOf(mapCleanMarker.getValue()) + 1;
+
+                    mapClean.put(mapCleanMarker.getKey(), subListMessage.get(x));
+                }
+            }
+        }
+
+        for (Map.Entry<String, String> mapCleanMarker : mapClean.entrySet()) {
+
+            if (listMessage.indexOf(mapCleanMarker.getValue()) != -1 && !mapKey.containsKey(mapCleanMarker.getKey())) {
+                mapClean.put(mapCleanMarker.getKey(), listMessage.get(listMessage.indexOf(mapCleanMarker.getValue()) + 1));
+
+            }
+        }
+
+        return mapClean;
     }
 
 }
